def5f799d768f7fc4d7bbfe3608624e75522e597
esmail elturky <eelturky@youxel.com>
2024-08-15 20:26:18 +0300

    adding the costumed header, change favicon  and setting angular.json amd tsconfig

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  @Input() template: TemplateRef<any>;
 }
--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
-
+  @Input() template: TemplateRef<any>;
 }
