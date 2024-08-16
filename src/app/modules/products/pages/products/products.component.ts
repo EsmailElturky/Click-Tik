77be68cade30c4641a37a949ae369068fee8f600
esmail elturky <eelturky@youxel.com>
2024-08-16 07:18:55 +0300

    adding ui of products page with dummy data thats includs filter of radio buttons and breadcrum and product card except pagination

--- src/app/modules/products/pages/products/products.component.ts
+++ src/app/modules/products/pages/products/products.component.ts
@@ -17,7 +17,33 @@ export class ProductsComponent implements OnInit, AfterViewInit {
   @ViewChild('headerTemplate') headerTemplate!: TemplateRef<any>;
   templateToPass!: TemplateRef<any>;
   count = 0;
+  category = 'smart phones';
+  searchText = 'Iphone';
+  categories = [
+    { name: 'All', count: 240 },
+    { name: 'Smartphones', count: 9 },
+    { name: 'Laptops', count: 12 },
+    { name: 'Fragrances', count: 8 },
+    { name: 'Skincare', count: 16 },
+    { name: 'Groceries', count: 12 },
+    { name: 'Home decoration', count: 4 },
+    { name: 'Furniture', count: 4 },
+    { name: 'Tops', count: 42 },
+    { name: 'Women’s dresses', count: 40 },
+    { name: 'Women’s shoes', count: 12 },
+    { name: 'Men’s shirts', count: 2 },
+    { name: 'Men’s shoes', count: 10 },
+    { name: 'Men’s watches', count: 15 },
+    { name: 'Women’s watches', count: 7 },
+    { name: 'Women’s bags', count: 9 },
+    { name: 'Women’s jewellery', count: 5 },
+    { name: 'Sunglasses', count: 13 },
+    { name: 'Automotive', count: 6 },
+    { name: 'Motorcycle', count: 14 },
+    { name: 'Lighting', count: 0 },
+  ];
 
+  selectedCategory = 'All';
   constructor() {}
 
   ngOnInit(): void {}
