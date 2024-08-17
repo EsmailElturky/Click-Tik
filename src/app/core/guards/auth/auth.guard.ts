8f8bb6b2f79a320fc344f2246eba03da6791fa5d
esmail elturky <eelturky@youxel.com>
2024-08-17 07:32:49 +0300

    adding Auth gard

---
+++ src/app/core/guards/auth/auth.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../../../modules/auth/services/auth-service/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.userDetails$.pipe(
+      map((userDetails) => {
+        if (userDetails) {
+          return true;
+        } else {
+          this.router.navigate(['/auth/sign-in']);
+          return false;
+        }
+      }),
+    );
+  }
+}
--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './pages/products/products.component';
+import { AuthGuard } from '../../core/guards/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: ProductsComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
